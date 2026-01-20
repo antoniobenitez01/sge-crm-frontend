e791a6186c27d0ad7ac80eec425638253e6d481d
antoniobenitez01 <anthonybenitezcontact@gmail.com>
2026-01-20 21:06:00 +0100

    Edited Enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,3 +3,5 @@ export const environment = {
 };
 export const URL_BASE = 'http://192.168.10.112/crm/';
 export const URL_API = `${URL_BASE}/api/private`;
+
+export const URL2_BASE = 'http://localhost:8000';
